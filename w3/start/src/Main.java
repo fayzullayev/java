49da84dc94d063feea9d12475cb7bd22d2c03cc9
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-10-23 21:46:34 +0500

    .

--- w3/start/src/Main.java
+++ w3/start/src/Main.java
@@ -1,13 +1,10 @@
-//https://www.w3schools.com/java/java_data_types_characters.asp
-
+// https://www.w3schools.com/java/java_data_types_characters.asp
 
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    boolean isJavaFun = true;
 
-        boolean isJavaFun = true;
-        System.out.println("isJavaFun: " + isJavaFun);
-
-
-    }
+    System.out.println("isJavaFun: " + isJavaFun);
+  }
 }
