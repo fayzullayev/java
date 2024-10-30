d6987bb2e97ed5b9edb3e10f72ec47bc900353c3
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-10-30 07:44:44 +0500

    .

--- w3/start/src/Main.java
+++ w3/start/src/Main.java
@@ -1,22 +1,20 @@
 // https://www.w3schools.com/java/java_data_types_characters.asp
 
-// time 8th;
-
+// time 8th 8:44 ;
 
 import human.Person;
 import people.People;
 
 class Main {
 
-    public static void main(String[] args) {
-//        vehicle.Vehicle vh = new vehicle.Vehicle();
+  public static void main(String[] args) {
+    //        vehicle.Vehicle vh = new vehicle.Vehicle();
 
-        People p = new People();
+    People p = new People();
 
-        System.out.println(p);
-//
-//        System.out.println(vh.);
+    System.out.println(p);
+    //
+    //        System.out.println(vh.);
 
-
-    }
+  }
 }
