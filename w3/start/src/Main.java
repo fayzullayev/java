9c789fdb9abd4af46f462a85cfe1e5ee2a2dfef1
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-11-06 23:08:40 +0500

    .

--- w3/start/src/Main.java
+++ w3/start/src/Main.java
@@ -1,25 +1,15 @@
 // https://www.w3schools.com/java/java_data_types_characters.asp
 
-// time 12th 24:44;
-
+// time 12th 30:12;
 
 class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        int a = 10;
+    int a = 10;
 
-        if (a < 20) {
-            System.out.println("a kichil 20 dan");
-        } else if (a == 100) {
-            System.out.println("a 100 ga teng");
-        } else {
-            System.out.println("Katta");
-        }
-
-    }
+    if (a < 20)
+      ;
+    System.out.println("fsfdfds");
+  }
 }
-
-
-
-
--- w3/start/src/references/Student.java
+++ w3/start/src/references/Student.java
@@ -1,41 +1,33 @@
 package references;
 
-import org.jetbrains.annotations.NotNull;
-
 class Student {
-    String name;
-    int course;
-    double grade;
+  String name;
+  int course;
+  double grade;
 
-    Student(String name, int course, double grade) {
-        this.name = name;
-        this.course = course;
-        this.grade = grade;
-    }
+  Student(String name, int course, double grade) {
+    this.name = name;
+    this.course = course;
+    this.grade = grade;
+  }
 
-    public static void swap(Student a, Student b) {
-        Student temp = a;
-        a = b;
-        b = temp;
-    }
+  public static void swap(Student a, Student b) {
+    Student temp = a;
+    a = b;
+    b = temp;
+  }
 
-    public static void changeName(@NotNull Student s) {
-        int t = "Mirodil".length() > 5 ? 0 : 1;
-    }
+  public static void changeName(Student s) {
+    int t = "Mirodil".length() > 5 ? 0 : 1;
+  }
 
-    public static void main(String[] args) {
-        Student a = new Student("John Doe", 3, 9.5);
+  public static void main(String[] args) {
+    Student a = new Student("John Doe", 3, 9.5);
 
-        System.out.println(a.name);
+    System.out.println(a.name);
 
-        Student.changeName(a);
+    Student.changeName(a);
 
-        System.out.println(a.name);
-
-
-    }
+    System.out.println(a.name);
+  }
 }
-
-
-
-
