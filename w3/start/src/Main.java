815ca594f973840ac19f17c320768f99c71e82cc
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-11-08 08:17:30 +0500

    .

--- w3/start/src/Main.java
+++ w3/start/src/Main.java
@@ -6,15 +6,24 @@ import car.Car;
 
 class Main {
 
-    public static void main(String[] args) {
-
-        Car c1 = new Car("red", "V3");
-        Car c2 = new Car("red", "V3");
-        Car c3 = c1;
-
-        if ("hello" == "hello") {
-            System.out.println("Equal");
-        }
-
+  static void maximum(int a, int b, int c) {
+    String str;
+    if (a > b && a > c) {
+      System.out.println(a + " is greater than b and c");
+    } else if (b > a && b > c) {
+      System.out.println(b + " is greater than a and c");
+    } else if (c > a && b < c) {
+      System.out.println(c + " is greater than b and a");
+    } else {
+      System.out.println("Teng");
     }
+    str = "ddd";
+    System.out.println(str);
+  }
+
+  public static void main(String[] args) {
+    String max = (4 > 5) ? "qwertyu" : "uytrewq";
+
+    System.out.println(max);
+  }
 }
