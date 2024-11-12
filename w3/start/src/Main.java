e606636212af946c608de43bd528452af36701de
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-11-12 18:53:12 +0500

    Update Main.java

--- w3/start/src/Main.java
+++ w3/start/src/Main.java
@@ -6,25 +6,12 @@ import car.Car;
 
 class Main {
 
-    static void maximum(int a, int b, int c) {
-        String str;
-
-        if (a > b && a > c) {
-            System.out.println(a + " is greater than b and c");
-        } else if (b > a && b > c) {
-            System.out.println(b + " is greater than a and c");
-        } else if (c > a && b < c) {
-            System.out.println(c + " is greater than b and a");
-        } else {
-            System.out.println("Teng");
-        }
-        str = "ddd";
-        System.out.println(str);
-    }
-
     public static void main(String[] args) {
-        String max = (4 > 5) ? "qwertyu" : "uytrewq";
 
-        System.out.println(max);
+
+        switch ("1233293840") {
+            case 1, 3:
+                break;
+        }
     }
 }
